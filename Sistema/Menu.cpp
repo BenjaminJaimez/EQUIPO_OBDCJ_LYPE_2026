47863264a68360dcdf72a7e62ff0172c249e73d9
BenjaminJaimez <castillo061201@gmail.com>
2026-05-22 10:33:49 -0600

    arreglos Menu.cpp 3

--- Sistema/Menu.cpp
+++ Sistema/Menu.cpp
@@ -1,13 +1,13 @@
 #include<string>
 #include <iostream>
 #include "Menu.h"
-#include "../services/ProductosService.h"
 using namespace std;
-
+int opcion;
 void Menu::mostrar(){
     cout << "----Menu Principal----" << endl;
     cout << "1. Agregar producto" << endl;
     cout << "2. Mostrar productos" << endl;
     cout << "3. Eliminar producto" << endl;
     cout << "Ingrese una opcion: ";
+    cin>> opcion;
 }
